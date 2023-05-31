1da8e170671809a9f8e7163649f9652bcebfbd89
Amandeep Singh <amandeep29032004@gmail.com>
2023-05-31 21:46:13 +0530

    sum of ll

---
+++ Linked_List/add_elements.cpp
@@ -0,0 +1,67 @@
+#include<iostream>
+using namespace std;
+
+struct Node{
+    int data ;
+    struct Node * next;
+}*first=NULL;
+    
+
+void createll(int A[],int n){
+    struct Node *t ,*last;
+    first = new Node;
+    first->data=A[0];
+    first->next=NULL;
+    last=first;
+
+    for (int i =1;i<n;i++){
+        t=new Node;
+        t->data=A[i];
+        t->next=NULL;
+        last->next=t;
+        last=t;
+    }
+
+}
+
+
+
+// <------------------------------------------>
+// void display(struct Node *p){
+//     while(p){
+//          cout<<p->data<<"\t";
+//          p=p->next;
+//     }
+// }
+// <------------------------------------------>
+
+
+
+// <-------------VIA RECURSSION REVERSE PRINTING--------------->
+void display(struct Node *p){
+    if(p!=NULL){
+         display(p->next);
+         cout<<p->data<<"\t";
+    }
+}
+// <------------------------------------------>
+int sumll(struct Node *p){
+    int sum=0;
+    while(p){
+        sum=sum+p->data;
+        p=p->next;
+    }
+    return(sum);
+}
+
+
+
+int main(){
+
+
+
+    int a[5]={0,55,88,44,66};
+    createll(a,5);
+    cout<< "sun is "<<sumll(first);
+    
+}
--- Linked_List/tempCodeRunnerFile.cpp
+++
@@ -1,7 +0,0 @@
-
-void display(struct Node *p){
-    while(p){
-         cout<<p->data<<"\t";
-         p=p->next;
-    }
-}
